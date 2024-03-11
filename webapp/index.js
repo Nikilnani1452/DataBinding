52897e27a1b8444e2e4ff8f610add4e94c1b4cfe
Nikilnani1452 <vishwanadulanikil@gmail.com>
2024-03-11 16:01:39 +0530

    step-12

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,18 @@
 sap.ui.require([
+	"sap/ui/core/Core",
+	"sap/ui/core/Messaging",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/mvc/XMLView",
 	"sap/ui/model/resource/ResourceModel"
-], function (JSONModel, XMLView, ResourceModel) {
+], function (Core, Messaging, JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
-	// Attach an anonymous function to the SAPUI5 'init' event
-	sap.ui.getCore().attachInit(function () {
+	// Chain an anonymous function to the SAPUI5 'ready' Promise
+	Core.ready().then(function () {
+		var oProductModel = new JSONModel();
+		oProductModel.loadData("./model/Products.json");
+		sap.ui.getCore().setModel(oProductModel, "products");
+
 		var oModel = new JSONModel({
 			firstName: "Harry",
 			lastName: "Hawk",
@@ -26,8 +32,8 @@ sap.ui.require([
 
 		var oResourceModel = new ResourceModel({
 			bundleName: "sap.ui.demo.db.i18n.i18n",
-			supportedLocales: ["", "de"],
-			fallbackLocale: ""			
+			fallbackLocale: "",
+			supportedLocales: ["", "de"]
 		});
 
 		sap.ui.getCore().setModel(oResourceModel, "i18n");
@@ -38,8 +44,7 @@ sap.ui.require([
 		});
 
 		// Register the view with the message manager
-		sap.ui.getCore().getMessageManager().registerObject(oView, true);
-
+		Messaging.registerObject(oView, true);
 
 		// Insert the view into the DOM
 		oView.placeAt("content");
