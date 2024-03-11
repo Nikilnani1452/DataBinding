376f698d22802a5c539e63ad559855f5e11b3c29
Nikilnani1452 <vishwanadulanikil@gmail.com>
2024-03-11 16:24:51 +0530

    step-13

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,7 +6,6 @@ sap.ui.define([
 	"sap/ui/model/type/Currency"
 ], function (Controller, mobileLibrary, Locale, LocaleData, Currency) {
 	"use strict";
-
 	return Controller.extend("sap.ui.demo.db.controller.App", {
 		formatMail: function(sFirstName, sLastName) {
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
@@ -16,8 +15,19 @@ sap.ui.define([
 				oBundle.getText("mailBody"));
 		},
 		formatStockValue: function(fUnitPrice, iStockLevel, sCurrCode) {
-			var oCurrency = new Currency();
+			var sBrowserLocale = sap.ui.getCore().getConfiguration().getLanguage();
+			var oLocale = new Locale(sBrowserLocale);
+			var oLocaleData = new LocaleData(oLocale);
+			var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
 			return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+		},
+		onItemSelected: function(oEvent) {
+			var oSelectedItem = oEvent.getSource();
+			var oContext = oSelectedItem.getBindingContext("products");
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("productDetailsPanel");
+			oProductDetailPanel.bindElement({ path: sPath, model: "products" });
+
 		}
 	});
 });
